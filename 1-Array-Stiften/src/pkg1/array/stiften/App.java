300ae1a2202c98a96427fb3dee49b6cf75c68415
Arjan <Arjan@Blokie-PC.lan>
2013-11-22 22:04:50 +0100

    First Commit

---
+++ 1-Array-Stiften/src/pkg1/array/stiften/App.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg1.array.stiften;
+
+/**
+ *
+ * @author Arjan
+ */
+public class App {
+           public static void main(String[] args) {
+                Deck deck = new Deck();
+                deck.fillDeck();
+                deck.shuffle();
+                
+                Card card = new Card(/*Bijvoorbeeld. Harten Aas */);
+                int testIndex = 30;
+                deck.insertAt(card, testIndex);
+                System.out.print("Sequential search ...");
+                int foundIndex = deck.sequentialSearch(card);
+                System.out.println((foundIndex == testIndex)?"Win":"Fail");
+                
+                System.out.print("Binary search...");
+                deck.sort();
+                foundIndex = deck.binarySearch(card);
+                System.out.println(foundIndex);
+
+                
+        }
+}
---
+++ 1-Array-Stiften/src/pkg1/array/stiften/Card.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg1.array.stiften;
+
+/**
+ *
+ * @author Arjan
+ */
+public class Card {
+            /**
+         * Klaver, Schoppen, Harten of Ruiten
+         * Hoeft niet persé van type Object te zijn,
+         * vul zelf in
+         */
+        Object suit;
+        
+        /**
+         * 2,3,4,5,6,7,8,9,10,b,v,k,a
+         * Hoeft niet persé van type Object te zijn,
+         * vul zelf in
+         */
+        Object number;
+        
+        /**
+         * Pretty-print deze Card als string
+         */
+        public String toString(){
+                return "";
+        }
+}
